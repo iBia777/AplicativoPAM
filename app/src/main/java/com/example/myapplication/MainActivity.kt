0535c3b78c87e1fcb2e39e11bd9c54d6c52c40f6
pcetec <pc@etec.com>
2026-09-16 20:46:32 -0300

    MANIPULANDO ELEMENTOS PELO KOTLIN

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.myapplication.R.id.editValor
 
 class MainActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.aula_id)
 
+        val botao = findViewById<Button>(R.id.btnProxima)
+
+        val texto = findViewById<EditText>(editValor)
+
+        botao.text = "Novo texto"
+
+        botao.setOnClickListener {
+
+            botao.text = texto.text
+
+
+            }
     }
 }
